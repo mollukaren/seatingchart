d6733a73e57ee3aba03abddc9ba6d07ec427c0ba
mollukaren <104541898+mollukaren@users.noreply.github.com>
2023-06-26 18:39:15 -0400

    adding Table SSR files, but tRPC is blocking functionality

---
+++ src/app/table/columns.tsx
@@ -0,0 +1,17 @@
+import type { ColumnDef } from "@tanstack/react-table"
+import { type CustomTable } from '@prisma/client';
+
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: ColumnDef<CustomTable>[] = [
+  {
+    accessorKey: "createdAt",
+    header: "Created Date",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,6 @@
-import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
+import { type AppType } from "next/app";
 import "@/styles/globals.css";
-
 import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
