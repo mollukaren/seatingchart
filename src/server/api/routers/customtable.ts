12ab6da246780975192fe22723d6ea5606a35542
mollukaren <104541898+mollukaren@users.noreply.github.com>
2023-06-28 17:35:20 -0400

    committing schema and form/api updates in parts (pt 1)

--- src/server/api/routers/customtable.ts
+++ src/server/api/routers/customtable.ts
@@ -1,20 +1,27 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
 
+export const customTableSchema = z.object({
+  airtable_token: z.string().length(82),
+  airtable_base: z.string().length(17),
+  airtable_table: z.string().length(17),
+  table_name: z.string().min(3)
+})
+
 export const customTableRouter = createTRPCRouter({
-  addTable: privateProcedure.input(z.object({
-    api_key: z.string().length(82),
-    table_name: z.string().min(3)
-  })
+  addTable: privateProcedure.input(
+    customTableSchema
   )
   .mutation(async ( { ctx, input } ) => {
 
     const userID = ctx.currentUserID;
     const newTable = await ctx.prisma.customTable.create({
-      data: {
+      data: { 
+        airtable_token: input.airtable_token,
+        airtable_base: input.airtable_base,
+        airtable_table: input.airtable_table,
         userID,
-        key: input.api_key,
-        name: input.table_name
+        name: input.table_name,
       },
     });
     return newTable;
