4f67b35ef321e1dea98e4957e4f66d004ce4b298
mollukaren <104541898+mollukaren@users.noreply.github.com>
2023-06-27 21:27:43 -0400

    adding column updates to prep for status icons

--- src/pages/tables/columns.tsx
+++ src/pages/tables/columns.tsx
@@ -5,6 +5,7 @@ import { type CustomTable } from '@prisma/client';
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
+
 export const columns: ColumnDef<CustomTable>[] = [
   {
     accessorKey: "createdAt",
