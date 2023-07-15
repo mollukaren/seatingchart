db76dcc320db553237e9e086501828823d1a6fb1
mollukaren <mollukaren@gmail.com>
2023-07-15 16:24:04 -0400

    finalizing light mode styling and adding shadcnui buttons to hero

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,6 +1,9 @@
 import { siteConfig } from '@/config/site'
 import { SignedIn, SignedOut } from '@clerk/nextjs'
 import React from 'react'
+import { Button } from './button'
+import Link from 'next/link'
+import { ChevronRight, Youtube } from 'lucide-react'
 
 const Hero = () => {
   return (
@@ -16,7 +19,7 @@ const Hero = () => {
           {/*  Buttons */}
           <div className="mt-7 grid gap-3 w-full sm:inline-flex">
           <SignedOut>
-            <a className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800" href="/sign-up">
+            <a href="/sign-up">
               Sign Up
               <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
                 <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
@@ -24,19 +27,17 @@ const Hero = () => {
             </a>
           </SignedOut>
           <SignedIn>
-          <a className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800" href="/dashboard">
-              Go To Dashboard
-              <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-              </svg>
-            </a>
+          <Button asChild>
+            <Link href="/dashboard">Login 
+              <ChevronRight />
+            </Link>
+          </Button>
           </SignedIn>
-            <a className="inline-flex justify-center items-center gap-x-3.5 text-sm lg:text-base text-center border hover:border-gray-300 shadow-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800" href={siteConfig.links.github}>
-              <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-              </svg>
-              Video Walkthrough
+            <Button variant="outline" asChild>
+            <a className='flex justify-content: space-between' href="www.youtube.com/somevideohere">
+              <Youtube /> &nbsp;&nbsp;Video Walkthrough
             </a>
+            </Button>
           </div>
           {/*  End Buttons */}
         </div>
