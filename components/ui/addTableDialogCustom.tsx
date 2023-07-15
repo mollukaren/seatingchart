59853cd0a039593e4c47cab69f1cf123cc4751c2
mollukaren <mollukaren@gmail.com>
2023-07-15 13:58:55 -0400

    light mode with initial colors built

--- components/ui/addTableDialogCustom.tsx
+++ components/ui/addTableDialogCustom.tsx
@@ -16,7 +16,7 @@ export function TableDialogCustom() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800">Add Table</Button>
+        <Button>Add Table</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
