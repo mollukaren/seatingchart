86bd63cfbb1a946646206f6d90511489eb1869d3
mollukaren <mollukaren@gmail.com>
2023-07-18 16:51:44 -0400

    favicon updates (not switching colors when system theme changes - stuck in dark mode)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,28 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { SiteFooter } from "@/components/ui/site-footer";
 import DashboardLayout from "@/components/ui/dynamicNavbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
 
     <ClerkProvider {...pageProps}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Head>
+          <title>Seating Cheart</title>
+          <link
+            href="logo_light.svg"
+            rel="icon"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            href="logo_dark.svg"
+            rel="icon"
+            media="(prefers-color-scheme: dark)"
+          />
+
+
+        </Head>
         <div className="flex min-h-screen flex-col space-y-6">
           <DashboardLayout/>
           <Component {...pageProps} />
